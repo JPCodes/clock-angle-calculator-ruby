74ca353deb4f91ecb29a7cf24ec3cb7f22eab851
Cody Brubaker and Jim Padilla <jim@>
2016-11-14 15:47:33 -0800

    fix rspec

--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -4,7 +4,7 @@ require('clock_angle')
 describe('String#clock_angle') do
 
   it('it will return the degrees for a given hour') do
-    expect("2.5".clock_angle()).to(eq([60, 180, 120]))
+    expect("2.5".clock_angle()).to(eq([75, 180, 105]))
   end
 
   it('it will return the degrees for the minutes') do
@@ -12,7 +12,7 @@ describe('String#clock_angle') do
   end
 
   it('it will return the degrees between the hour and minute hands') do
-    expect('9.5'.clock_angle()).to(eq([270, 180, 90]))
+    expect('9.5'.clock_angle()).to(eq([285, 180, 105]))
   end
 
 end
